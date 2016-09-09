408ae5cbf541e8462bb6eb3fb2bdfe027a368b51
sareduhh <sarahe.kaminski@gmail.com>
2016-09-08 21:38:52 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No_Input_Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 1;
+            for (counter = 1; counter <= 99; counter += 2)
+            {
+                System.Console.WriteLine(counter);
+            }
+
+        }
+    }
+}
